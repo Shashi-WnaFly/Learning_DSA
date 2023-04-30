fa0fb869dbe6ffc8825fd0f71e7e38f2375bb63a
Shashi <shashianand2600@gmail.com>
2023-04-30 09:35:38 +0530

    a program based on encription.

---
+++ Encripter.java
@@ -0,0 +1,30 @@
+package DSA;
+
+import java.util.Scanner;
+// this is the program based on encription when user input a string is converted into 3 letters ahead an encription password.
+public class Encripter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter any String.");
+        String pass = in.next();
+        String encrip = "";
+        char s;
+        int a, i;
+        for(i = 0; i<pass.length(); i++){
+            s = pass.charAt(i);
+            a = (int)s + 3;
+            if(s>=97 && s<=119){
+                encrip += (char)a;
+            }
+            else if(s>119 && s<=122) {
+                a = ((int)s + 3 )- 26;
+                encrip += (char) a;
+            }
+            if(s>122){
+                a = (int)s - 26;
+                encrip += (char)a;
+            }
+        }
+            System.out.println(encrip);
+    }
+}
