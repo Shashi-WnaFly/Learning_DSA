ed7af73282da09759d5f292b0b8daf95f7756658
Shashi <shashianand2600@gmail.com>
2023-05-01 09:05:34 +0530

    practise of a boolean method.

--- Primex.java
+++ Primex.java
@@ -1,29 +1,31 @@
 package DSA;
+
 import java.util.Scanner;
 
 public class Primex {
-    static boolean prime(int p){
+    static boolean prime(int p) {
         int i;
         boolean prich;
 
-        if (p<=1) return false;
-        for(i=2; i*i<=p; i++){
-            if(p%i==0){
+        if (p <= 1) return false;
+        for (i = 2; i * i <= p; i++) {
+            if (p % i == 0) {
                 prich = false;
                 return prich;
             }
         }
-        return i*i>p;
+        return i * i > p;
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter any number to prime or not.");
 //        int n = in.nextInt();
         int i, count = 0;
-        for (i = 1; i<=1000; i++){
+        for (i = 1; i <= 1000; i++) {
             boolean check = prime(i);
-            if(check){
-                System.out.print(i+ ", ");
+            if (check) {
+                System.out.print(i + ", ");
                 count++;
             }
         }
