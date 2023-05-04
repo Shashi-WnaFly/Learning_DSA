65301693725f6b709e7eada646bfff2b593416fc
Shashi <shashianand2600@gmail.com>
2023-05-04 09:45:08 +0530

    building an Array from permutation.

---
+++ Solution.java
@@ -0,0 +1,20 @@
+package DSA;
+import java.util.*;
+// build an array from permutation.
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] goa = new int[10];
+        for (int i = 0; i < goa.length; i++) {
+            goa[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(buildArray(goa)));
+    }
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
