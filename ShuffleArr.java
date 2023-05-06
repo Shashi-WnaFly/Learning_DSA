68b5c1ab6073990af158c06812e3d5c035c493e1
Shashi <shashianand2600@gmail.com>
2023-05-06 10:02:00 +0530

    shuffle the array.

---
+++ ShuffleArr.java
@@ -0,0 +1,23 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class ShuffleArr {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 3, 4, 7};
+        int n = 3;
+        System.out.println(Arrays.toString(shuffle(arr, n)));
+    }
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        int i=0, k=0;
+        while(i < nums.length){
+            ans[i] = nums[k];
+            ans[i+1] = nums[n];
+            i += 2;
+            k++;
+            n++;
+        }
+        return ans;
+    }
+}
