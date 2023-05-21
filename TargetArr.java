a68cc495dfde002e01a184722f498697882363f2
Shashi <shashianand2600@gmail.com>
2023-05-21 11:41:28 +0530

    day 17 : Without ArrayList, create a new array with given index array with value of num and shift the value of target array.

--- TargetArr.java
+++ TargetArr.java
@@ -11,26 +11,24 @@ public class TargetArr {
         int[] target = new int[nums.length];
         int[] copy = new int[nums.length];
 
+        for(int i = 0; i<nums.length; i++){
+            int j = index[i];
+            if(i > j){
+                for(int k = 0; k<nums.length; k++) copy[k] = target[k];
+                target[j] = nums[i];
+                for(j=index[i]; j<nums.length-1; j++){
+                    target[j+1] = copy[j];
+                }
+            }
+            target[index[i]] = nums[i];
+        }
+
 //        for(int i = 0; i<nums.length; i++){
 //            for(int j = i; j > index[i]; j--){
 //                target[j] = target[j-1];
 //            }
 //            target[index[i]] = nums[i];
 //        }
-
-        for(int i = 0; i<nums.length; i++){
-            int j = index[i];
-            if(i > j){
-                for(int k = 0; k<nums.length; k++){
-                    copy[k] = target[k];
-                }
-                target[j] = nums[i];
-                for(j=index[i]; j<nums.length-1; j++){
-                    target[j+1] = copy[j];
-                }
-            }
-                target[index[i]] = nums[i];
-        }
         return target;
     }
 //    public static int[] createTargetArray(int[] nums, int[] index){
