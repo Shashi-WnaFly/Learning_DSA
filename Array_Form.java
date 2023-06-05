5d26ef739c02cea20e4bfe7123410cad1dc3de35
Shashi <shashianand2600@gmail.com>
2023-06-05 06:36:05 +0530

    i will try this question again after 5 days.

--- Array_Form.java
+++ Array_Form.java
@@ -3,50 +3,54 @@ package DSA;
 import java.util.*;
 // Add To Array-Form Of Integer.
 public class Array_Form {
-    public static int count(long l){
-        int c = 0;
-        while(l>0){
-            l/=10;
-            c++;
-        }
-        return c;
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+
     }
-    public static List<Integer> addToArrayForm(int[] num, int k){
-        List<Integer> sum = new ArrayList<>();
-
-        long res = 0;
-
-        for(int i=0; i<num.length; i++){
-            res = (res*10)+num[i];
-        }
-
-        res += k;
-        int l = count(res);
-        long[] narr = new long[l];
-
-        k=0;
-        while(res>0){
-            narr[k] = res%10;
-            res /= 10;
-            k++;
-        }
-
-        int j = l-1;
-
-        for(int i = 0; i<l; i++){
-            sum.add(i, (int)narr[j]);
-            j--;
-        }
-
-        System.gc();
-        return sum;
-    }
+    // My solution after trying much time.
+//    public static int count(long l){
+//        int c = 0;
+//        while(l>0){
+//            l/=10;
+//            c++;
+//        }
+//        return c;
+//    }
+//    public static List<Integer> addToArrayForm(int[] num, int k){
+//        List<Integer> sum = new ArrayList<>();
+//
+//        long res = 0;
+//
+//        for(int i=0; i<num.length; i++){
+//            res = (res*10)+num[i];
+//        }
+//
+//        res += k;
+//        int l = count(res);
+//        long[] narr = new long[l];
+//
+//        k=0;
+//        while(res>0){
+//            narr[k++] = res%10;
+//            res /= 10;
+//        }
+//
+//        int j = l-1;
+//
+//        for(int i = 0; i<l; i++){
+//            sum.add(i, (int)narr[j--]);
+//        }
+//        System.gc();
+//        return sum;
+//    }
 
     public static void main(String[] args) {
-        int[] num = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-        int k = 1;
+//        int[] num = {1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3};
+//        int k = 516;
 
+        int[] num = {2, 1, 5};
+        int k = 806;
         System.out.println(addToArrayForm(num, k));
         System.out.println(Long.MAX_VALUE);
+        //[1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,5,7,9]
     }
 }
