0b2ec0004d0403f25bdffa57a16a083b2aabbaa6
Shashi <shashianand2600@gmail.com>
2023-06-06 13:10:24 +0530

    solving the same question with different method.

--- PlusOne.java
+++ PlusOne.java
@@ -5,7 +5,24 @@ import java.util.Arrays;
 public class PlusOne {
     public static int[] plusOne(int[] digits) {
 
-// this solution is my first approach.
+//   --> this is my learn approach from others. <--
+        for(int i = digits.length-1; i>=0; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                break;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        if(digits[0] == 0){
+            int[] res = new int[digits.length+1];
+            res[0] = 1;
+            return res;
+            }
+        return digits;
+
+//          *** this solution is my first approach. ***
 //        int[] Plus1 = new int[digits.length];
 //        int l = digits.length-1;
 //        int carry = 1, sum = 0;
