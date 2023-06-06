4f48342882d7dfcd48aac0ef168d0deeb188e5b5
Shashi <shashianand2600@gmail.com>
2023-06-06 12:30:50 +0530

    first time i use the carry method for solving this type of question.

---
+++ PlusOne.java
@@ -0,0 +1,35 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+
+// this solution is my first approach.
+//        int[] Plus1 = new int[digits.length];
+//        int l = digits.length-1;
+//        int carry = 1, sum = 0;
+//
+//        for(int i = 0; i<digits.length; i++){
+//            sum = carry + digits[l];
+//            Plus1[l--] = sum%10;
+//            carry = sum/10;
+//            sum /= 10;
+//        }
+//
+//        if(carry != 0){
+//            digits = new int[digits.length+1];
+//            digits[0] = carry;
+//            for(int i = 1; i<digits.length; i++){
+//                digits[i] = Plus1[i-1];
+//            }
+//            return digits;
+//        }
+//        return Plus1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(arr)));
+    }
+}
