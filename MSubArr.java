7eec677cddb6684c6205315da4efc6b9b1a71441
Shashi <shashianand2600@gmail.com>
2023-06-14 20:02:11 +0530

    This is my second solution after seeing the solution on leetcode.

--- MSubArr.java
+++ MSubArr.java
@@ -1,23 +1,22 @@
 package DSA;
 
 public class MSubArr {
-    public static int maxSubArray(int[] nums){
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++){
-            int sum = 0;
-            for(int j = i; j<nums.length; j++){
+    public static int maxSubArray(int[] nums) {
 
-                sum += nums[j];
-                if (max < sum){
-                    max = sum;
-                }
-            }
+        int max = Integer.MIN_VALUE, sum = 0, i = 0, l = nums.length;
+        for (; i < l; i++) {
+
+
+            sum += nums[i];
+            max = Math.max(sum, max);
+
+            if (0 > sum) sum = 0;
         }
-    return max;
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] net = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(net));
+        int[] num = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(num));
     }
 }
