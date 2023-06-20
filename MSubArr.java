4280d55aec6dc6213f91e26a4a61ead8fe6a4747
Shashi <shashianand2600@gmail.com>
2023-06-20 22:44:04 +0530

    commit correction

--- MSubArr.java
+++ MSubArr.java
@@ -1,20 +1,20 @@
 package DSA;
 // Leetcode --> 53. Maximum Subarray
 
-public class MSubArr {
-    int max = Integer.MIN_VALUE, i = 0, sum = 0, l = nums.length;
-        for(; i<l; i++){
-
-                sum += nums[i];
-                max = Math.max(sum, max);
-
-                if(sum<0) sum = 0;
-            }
-        return max;
-    }
-
-    public static void main(String[] args) {
-        int[] net = {-2,1,-3,4,-1,2,1,-5,3};
-        System.out.println(maxSubArray(net));
-    }
-}
+//public class MSubArr {
+//    int max = Integer.MIN_VALUE, i = 0, sum = 0, l = nums.length;
+//        for(; i<l; i++){
+//
+//                sum += nums[i];
+//                max = Math.max(sum, max);
+//
+//                if(sum<0) sum = 0;
+//            }
+//        return max;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] net = {-2,1,-3,4,-1,2,1,-5,3};
+//        System.out.println(maxSubArray(net));
+//    }
+//}
