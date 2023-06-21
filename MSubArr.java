3134387d5d827bc0a9bd0fa6aaad0047f04d4f6b
Shashi <shashianand2600@gmail.com>
2023-06-21 06:57:34 +0530

    cordinating the remote and local repository.

--- MSubArr.java
+++ MSubArr.java
@@ -1,20 +1,21 @@
 package DSA;
-// Leetcode --> 53. Maximum Subarray
 
-//public class MSubArr {
-//    int max = Integer.MIN_VALUE, i = 0, sum = 0, l = nums.length;
-//        for(; i<l; i++){
-//
-//                sum += nums[i];
-//                max = Math.max(sum, max);
-//
-//                if(sum<0) sum = 0;
-//            }
-//        return max;
-//    }
-//
-//    public static void main(String[] args) {
-//        int[] net = {-2,1,-3,4,-1,2,1,-5,3};
-//        System.out.println(maxSubArray(net));
-//    }
-//}
+// Leetcode --> 53. Maximum Subarray
+public class MSubArr {
+    public static int maxSubArray(int[] nums){
+    int max = Integer.MIN_VALUE, i = 0, sum = 0, l = nums.length;
+        for(; i<l; i++){
+
+                sum += nums[i];
+                max = Math.max(sum, max);
+
+                if(sum<0) sum = 0;
+            }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] net = {-2,1,-3,4,-1,2,1,-5,3};
+        System.out.println(maxSubArray(net));
+    }
+}
