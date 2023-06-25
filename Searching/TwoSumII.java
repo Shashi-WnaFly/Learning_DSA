e69199f72b925b3e687921238b4160ddfb8f87bc
Shashi <shashianand2600@gmail.com>
2023-06-25 12:49:51 +0530

    this problem is solved. using this is unique, i think this is the two pointer approach.

---
+++ Searching/TwoSumII.java
@@ -0,0 +1,26 @@
+package Learning_DSA.Searching;
+
+import java.util.Arrays;
+
+public class TwoSumII {
+        public static int[] twoSum(int[] numbers, int target) {
+            int start = 0, end = numbers.length-1;
+
+            while(start < end){
+
+                if(numbers[start] + numbers[end] == target) return new int[] {start+1, end+1};
+
+                else if(numbers[start] + numbers[end] < target) start++;
+
+                else {
+                    end--;
+                }
+            }
+            return null;
+        }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4};
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
+    }
+}
