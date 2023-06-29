d38892f74bef4d555f060ee4bccdc881d22a2ba5
Shashi <shashianand2600@gmail.com>
2023-06-29 08:20:10 +0530

    this array is two directional sorted. so find the maximum value which is greater than next value.

---
+++ Searching/MountainIndex.java
@@ -0,0 +1,29 @@
+package Learning_DSA.Searching;
+
+public class MountainIndex {
+    public static int peakIndexInMountainArray(int[] arr) {
+
+        int start = 0, end = arr.length-1, mid;
+        int max = 0;
+
+        while(start <= end){
+
+            mid = start + (end - start)/2;
+
+            if(arr[mid] >= arr[max] && arr[mid] > arr[mid+1]){
+                max = mid;
+                end = mid - 1;
+            }
+
+            else{
+                start = mid + 1;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {24,69,100,99,79,78,67,36,26,19};
+        System.out.println(peakIndexInMountainArray(array));
+    }
+}
