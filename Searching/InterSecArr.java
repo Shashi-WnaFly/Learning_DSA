b9ad2a02eeaa950393bd40a68ef51f216c813f39
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-15 10:34:32 +0530

    Update InterSecArr.java
    applying some changes for program to use less space.

--- Searching/InterSecArr.java
+++ Searching/InterSecArr.java
@@ -1,17 +1,17 @@
-package learning_DSA.Searching;]
+package Learning_DSA.Searching;]
 
 // Leetcode --> 349. Intersection of Two Arrays.
 class InterSecArr {
     public int[] intersection(int[] nums1, int[] nums2) {
 
         int[] temp = new int[1001];
-        int cnt = 0;
+        int cnt = 0, i = 0, j = 0;
 
-        for(int i = 0; i < nums1.length; i++){
+        for(; i < nums1.length; i++){
             temp[nums1[i]] = 1;  // value of nums1 array used as temp array index and initialize by 1.
         }
 
-        for(int i = 0; i < nums2.length; i++){
+        for(i = 0; i < nums2.length; i++){
             if(temp[nums2[i]] == 1){
                 temp[nums2[i]] = 2; // value of nums2 array used as temp array index and initialize by 2.
                 cnt++;
@@ -19,12 +19,13 @@ class InterSecArr {
         }
        
         int[] intersectionArr = new int[cnt];
-        for(int i = 0, j = 0; i < temp.length && j < cnt; i++){
+        for(i = 0; i < temp.length && j < cnt; i++){
             if(temp[i] == 2){
                 intersectionArr[j] = i; // adding of temp index if temp[i] match with value 2.
                 j++;
             } 
         }
+        System.gc();
         return intersectionArr;
     }
 }
