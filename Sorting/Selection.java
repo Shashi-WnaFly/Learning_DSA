a811209d05265a056544f6c45af56dc4031d3c97
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-07-27 09:29:15 +0530

    first program with selection sort

---
+++ Sorting/Selection.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class HelloWorld{
+    public static void sort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            
+            int max = -65000;
+            int index = -1;
+            
+            for(int j = 0; j < arr.length - i; j++){
+                if(arr[j] > max){
+                    max = arr[j];
+                    index = j;
+                }
+            }
+            int temp = arr[index];
+            arr[index] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+    }
+
+     public static void main(String []args){
+         int[] arr = {2, 23, 11, 5, -19, 0};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+     }
+}
