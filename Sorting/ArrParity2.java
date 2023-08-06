9c6bb24fbbb0df95e00503c341d180e127faddb9
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-06 09:32:30 +0530

    Create ArrParity2.java first approach or normal approach

---
+++ Sorting/ArrParity2.java
@@ -0,0 +1,23 @@
+package Learning_DSA.Sorting;
+// Leetcode --> 922. Sort Array By Parity II
+
+class Solution {
+    
+    public int[] sortArrayByParityII(int[] nums) {
+
+        int[] arr = new int[nums.length];
+
+        int j = 0, k = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 != 0){
+                arr[k] = nums[i];
+                k += 2;
+            }
+            else{
+                arr[j] = nums[i];
+                j += 2;
+            }
+        }
+        return arr;
+    }
+}
