1a864ea619ebf177b1659d1cdbe92321781d9267
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-08 09:07:56 +0530

    first approach

---
+++ Sorting/SqSortedArr.java
@@ -0,0 +1,23 @@
+package Learning_DSA.Sorting;
+// Leetcode --> 977. Squares of a Sorted Array
+// it takes 220ms and 45.3mb
+class SqSortedArr {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i]*nums[i];
+        }
+
+        for(int i = 0; i < (n-1); i++){
+            for(int j = i+1; j > 0; j--){
+                if(nums[j] < nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+                else break;
+            }
+        }
+        return nums;
+    }
+}
