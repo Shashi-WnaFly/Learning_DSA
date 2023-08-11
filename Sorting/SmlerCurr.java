55749a892e5a377d0a238bd15204439c92953f94
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-11 10:58:21 +0530

    Create SmlerCurr.java solved

---
+++ Sorting/SmlerCurr.java
@@ -0,0 +1,34 @@
+package Learning_DSA.Sorting;
+// Leetcode --> 1365. How Many Numbers Are Smaller Than the Current Number
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int i, j, cnt, n = nums.length;
+        int[] Snum = nums.clone();
+
+        for(i = 0; i < n-1; i++){
+            for(j = i+1; j > 0; j--){
+                if(Snum[j] < Snum[j-1]){
+                    int temp = Snum[j];
+                    Snum[j] = Snum[j-1];
+                    Snum[j-1] = temp;
+                }
+                else break;
+            }
+        }
+
+        for(i = 0; i < n; i++){
+             cnt = 0;
+
+            for(j = 0; j < n; j++){
+                if(nums[i] > Snum[j]) cnt++;
+                
+                else break;
+            }
+            
+            nums[i] = cnt;
+        }
+        System.gc();
+        return nums;
+    }
+}
