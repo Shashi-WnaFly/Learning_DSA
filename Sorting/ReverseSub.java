7744ea1ac50f9850d20be3581783ae525d085205
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-12 10:17:37 +0530

    Update ReverseSub.java with second approach

--- Sorting/ReverseSub.java
+++ Sorting/ReverseSub.java
@@ -2,23 +2,36 @@ package Learning_DSA.Sorting;
 // Leetcode --> 1460. Make Two Arrays Equal by Reversing Subarrays
 import java.util.Arrays;
 class Solution {
-    public static void sort(int[] arr){
-        int n = arr.length, temp;
-        for(int i = 0; i < n-1; i++){
-            for(int j = i+1; j > 0; j--){
-                if(arr[j] < arr[j-1]){
-                    temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }
-                else break;
-            }
-        }
-    }
+    // public static void sort(int[] arr){
+    //     int n = arr.length, temp;
+    //     for(int i = 0; i < n-1; i++){
+    //         for(int j = i+1; j > 0; j--){
+    //             if(arr[j] < arr[j-1]){
+    //                 temp = arr[j];
+    //                 arr[j] = arr[j-1];
+    //                 arr[j-1] = temp;
+    //             }
+    //             else break;
+    //         }
+    //     }
+    // }
     public boolean canBeEqual(int[] target, int[] arr) {
-        if(Arrays.equals(arr, target)) return true;
-        sort(arr);
-        sort(target);
-        return Arrays.equals(arr, target);
+        // if(Arrays.equals(arr, target)) return true;
+        // sort(arr);
+        // sort(target);
+        // return Arrays.equals(arr, target);
+
+        // Second approach
+        int MA= 1, n = target.length;
+        int MT = 1, SA = 0, ST = 0;
+
+        for(int i = 0; i < n; i++){
+            MA *= arr[i];
+            MT *= target[i];
+            SA += arr[i];
+            ST += target[i];
+        }
+
+        return ((MA == MT) && (SA == ST));
     }
 }
