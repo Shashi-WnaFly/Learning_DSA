3e82c2b0e58b82be5975a364edaee9e282301f42
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-22 11:11:14 +0530

    Update RobotMove.java second approach

--- RobotMove.java
+++ RobotMove.java
@@ -2,18 +2,25 @@ package Learning_DSA.String;
 // Leetcode --> 657. Robot Return to Origin
 class RobotMove {
     public boolean judgeCircle(String moves) {
-        int L = 0, U = 0, D = 0, R = 0, n = moves.length();
-        for(int i = 0; i < n; i++){
-            switch(moves.charAt(i)){
-                case 'L': L++;
-                break;
-                case 'D': D++;
-                break;
-                case 'U': U++;
-                break;
-                case 'R': R++;
-            }
+        // int L = 0, U = 0, D = 0, R = 0, n = moves.length();
+        // for(int i = 0; i < n; i++){
+        //     switch(moves.charAt(i)){
+        //         case 'L': L++;
+        //         break;
+        //         case 'D': D++;
+        //         break;
+        //         case 'U': U++;
+        //         break;
+        //         case 'R': R++;
+        //     }
+        // }
+        // return (L == R && U == D);
+        // ***** second solution ************
+        int[] s = new int[21];
+        char[] moc = moves.toCharArray();
+        for(char c : moc){
+            s[c - 65]++;
         }
-        return (L == R && U == D);
+        return (s[11] == s[17] && s[20] == s[3]);
     }
 }
