c530d74f2892e4487aa427786244d24cabfcf1ba
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-22 09:02:57 +0530

    Create CntSubS.java first approach

---
+++ CntSubS.java
@@ -0,0 +1,25 @@
+package Learning_DSA.String;
+// Leetcode --> 1967. Number of Strings That Appear as Substrings in Word
+class CntSubS {
+    public boolean sub(String b, String c){
+        int bl = b.length(), cl = c.length();
+        for(int i = 0; i < bl; i++){
+            if(c.charAt(0) == b.charAt(i)){
+                try{
+                    if(c.equals(b.substring(i, (cl+i)))) return true;
+                }catch (Exception e){
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    public int numOfStrings(String[] patterns, String word) {
+        int cnt= 0;
+        for(String i : patterns){
+            boolean ch = sub(word, i);
+            if(ch) cnt++;
+        }
+        return cnt;
+    }
+}
