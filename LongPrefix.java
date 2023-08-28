5cf29d6c0fa7195eddeff7f666d691d673dc9fdb
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-08-28 08:08:04 +0530

    Create LongPrefix.java 1ms solution

---
+++ LongPrefix.java
@@ -0,0 +1,30 @@
+package Learning_DSA.String;
+// Leetcode --> 14. Longest Common Prefix
+
+class LongPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        int n = strs.length;
+        StringBuilder sb = new StringBuilder();
+
+        if(strs[0].equals("")) return "";
+        for(int i = 0; i < strs[0].length(); i++){
+
+            int cnt = 0;
+            char c = strs[0].charAt(i);
+
+            for(int j = 0, k = i; j < n && k < strs[j].length(); j++){
+                if(strs[j].charAt(k) == c){
+                    cnt++;
+                }
+            }
+
+            if(cnt == n){
+                sb.append(strs[0].charAt(i));
+            }
+            else return sb.toString();
+
+        }
+        return sb.toString();
+    }
+}
