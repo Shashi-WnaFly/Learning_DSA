28c0997f10472ec33932fe38bceeb0686f487d13
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2023-11-27 20:33:02 +0530

    second approach takes 4ms

--- bitWise/Consistent.java
+++ bitWise/Consistent.java
@@ -1,19 +1,38 @@
 // Leetcode --> 1684. Count the Number of Consistent Strings
 class Consistent {
     public int countConsistentStrings(String allowed, String[] words) {
-        int res = 0;
-        int l = words.length;
+        // int res = 0;
+        // int l = words.length;
+        // int ls = allowed.length();
+        // for(int i = 0; i < l; i++){
+        //     int li = words[i].length();
+        //     int match = 0;
+        //     for(int j = 0; j < li; j++){
+        //         for(int k = 0; k < ls; k++){
+        //             if(words[i].charAt(j) == allowed.charAt(k)) match++;
+        //         }
+        //     }
+        //     if(match == li) res++;
+        // }
+        // return res;
+
+        // second Approach
+        int cnt = 0;
         int ls = allowed.length();
-        for(int i = 0; i < l; i++){
-            int li = words[i].length();
-            int match = 0;
-            for(int j = 0; j < li; j++){
-                for(int k = 0; k < ls; k++){
-                    if(words[i].charAt(j) == allowed.charAt(k)) match++;
+        int[] arr = new int[26];
+        for(int i = 0; i < ls; i++){
+            arr[allowed.charAt(i) - 'a']++; 
+        }
+        for(String word : words){
+            int l = word.length();
+            for(int i = 0; i < l; i++){
+                if(arr[word.charAt(i) - 'a'] == 0){
+                    cnt++;
+                    break;
                 }
             }
-            if(match == li) res++;
         }
-        return res;
+        
+        return words.length - cnt;
     }
 }
