602592f60aa6f52de00b732d198ff91719df5d1d
Shashi-Anand <124153986+Shashi-WnaFly@users.noreply.github.com>
2024-01-17 11:27:02 +0530

    second and little different approach

--- Recursion/TriSum.java
+++ Recursion/TriSum.java
@@ -25,4 +25,32 @@ class TriSum {
 
         return sumTri(res, arr, --n, len);
     }
+    
+}
+    // little different approach
+class Compute {
+    static ArrayList<Long> list = new ArrayList<>();
+    
+    public long[] getTriangle(long arr[], long n)
+    {
+        for(int i = (int)n-1; i >= 0; --i){
+            list.add(arr[i]);
+        }
+
+        for(int i = 0; i < n-1; i++){
+            arr[i] = arr[i]+arr[i+1];
+        }
+
+        if(n > 1) return getTriangle(arr, --n);
+
+        int s = list.size();
+        long[] res = new long[s];
+        int j = 0;
+        
+        for(int i=s-1; i>=0; --i){
+            res[j++] = list.get(i);
+        }
+        list.clear();
+        return res;
+    }
 }
